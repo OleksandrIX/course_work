169f6b74420c504a372249ff1e5ca3f6ca80fc56
OleksandrIX <zagorenkosasha@gamil.com>
2023-06-09 15:18:12 +0300

    Created accounting-servicemen-in-garrison project

---
+++ src/config/passport.js
@@ -0,0 +1,20 @@
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+
+module.exports = (passport, getUserByUsername, getUserById) => {
+    const authenticateUser = async (username, password, done) => {
+        try {
+            const user = await getUserByUsername(username);
+            if (!user) return done(null, false, {message: "Такого користувача немає"});
+
+            if (await bcrypt.compare(password, user.password)) return done(null, user);
+            else return done(null, false, {message: "Невірний пароль"});
+        } catch (error) {
+            return done(error);
+        }
+    };
+
+    passport.use(new LocalStrategy({usernameField: "username"}, authenticateUser));
+    passport.serializeUser((user, done) => done(null, user.id));
+    passport.deserializeUser(async (id, done) => done(null, getUserById(id)));
+};
